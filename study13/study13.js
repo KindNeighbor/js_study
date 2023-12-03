d4b0286fd74691806a54d6144bdb6fc5c455c2c3
YJ <zerosix1017@gmail.com>
2023-12-03 16:35:05 +0900

    study13 #1

---
+++ study13/study13.js
@@ -0,0 +1,58 @@
+$.get('https://codingapple1.github.io/hello.txt')
+  .then(function(data){
+    console.log(data)
+  })
+  .catch(function(error){
+    console.log('실패함')
+  });
+
+  $.get('https://codingapple1.github.io/price.json')
+  .then(function(data){
+    console.log(data)
+  })
+  .catch(function(error){
+    console.log('실패함')
+  });
+
+  fetch('https://codingapple1.github.io/price.json')
+  .then(res => res.json())
+  .then(function(data){
+    console.log(data)
+  })
+  .catch(function(error){
+    console.log('실패함')
+  });
+
+  var products = [
+    {id: 0, price: 70000, title: 'Blosoom Dress'},
+    {id: 1, price: 50000, title: 'Springfield Shirt'},
+    {id: 2, price: 60000, title: 'Black Monastery'}
+  ]
+
+  products.forEach((a, i)=>{
+    var 템플릿 = 
+    `<div class="col-sm-4">
+      <img src="https://via.placeholder.com/600" class="w-100">
+      <h5>${products[i].title}</h5>
+      <p>가격 : ${products[i].price}</p>
+    </div>`;
+    $('.row').append(템플릿)
+});
+
+$('#more').click(function(){
+  $.get('https://codingapple1.github.io/js/more1.json')
+    .done((data)=>{
+      
+      data.forEach((a, i)=>{
+        var 템플릿 = 
+        `<div class="col-sm-4">
+          <img src="https://via.placeholder.com/600" class="w-100">
+          <h5>${data[i].title}</h5>
+          <p>가격 : ${data[i].price}</p>
+        </div>`;
+        $('.row').append(템플릿)
+      })
+      
+    });
+});
+
