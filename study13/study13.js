c34fc97c34516570c1289a6caf28961a264d9966
YJ <zerosix1017@gmail.com>
2023-12-03 16:49:17 +0900

    study13 #5

--- study13/study13.js
+++ study13/study13.js
@@ -23,38 +23,37 @@ $.get('https://codingapple1.github.io/hello.txt')
     console.log('실패함')
   });
 
-  var products = [
-    {id: 0, price: 70000, title: 'Blosoom Dress'},
-    {id: 1, price: 50000, title: 'Springfield Shirt'},
-    {id: 2, price: 60000, title: 'Black Monastery'}
-  ]
+var products = [
+  {id: 0, price: 70000, title: 'Blosoom Dress'},
+  {id: 1, price: 50000, title: 'Springfield Shirt'},
+  {id: 2, price: 60000, title: 'Black Monastery'}
+]
 
-  products.forEach((a, i)=>{
-    var 템플릿 = 
+function renderProducts(products) {
+  products.forEach((product) => {
+    var template = 
     `<div class="col-sm-4">
       <img src="https://via.placeholder.com/600" class="w-100">
-      <h5>${products[i].title}</h5>
-      <p>가격 : ${products[i].price}</p>
+      <h5>${product.title}</h5>
+      <p>가격 : ${product.price}</p>
     </div>`;
-    $('.row').append(템플릿)
-});
+
+    $('.row').append(template);
+  });
+}
+
+renderProducts(products);
 
 let cnt = 1;
 
 $('#more').click(function(){
   const url = `https://codingapple1.github.io/js/more${cnt}.json`;
   $.get(url)
+  
   .done((data)=>{      
-    data.forEach((a, i)=>{
-      var 템플릿 = 
-        `<div class="col-sm-4">
-        <img src="https://via.placeholder.com/600" class="w-100">
-        <h5>${data[i].title}</h5>
-        <p>가격 : ${data[i].price}</p>
-        </div>`;
-        $('.row').append(템플릿)
-      })  
+    renderProducts(data);
     });
+    
   if (cnt == 2) {
     $('#more').css("display", 'none');
   } else {
